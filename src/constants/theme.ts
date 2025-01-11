32da8bfc00c3a7c9c7fe42a3b913dc409d4bdb48
Schnee <jooeun06161@gmail.com>
2025-01-11 20:53:11 +0900

    패키지 및 설정, UI 테마 토큰 추가 (#2)
    * chore: 패키지, 린트, prettier 설정 추가
    
    * chore: emotion 추가
    
    * chore: prettier 설정 추가
    
    * feat: css 변수 및 토큰 추가
    
    * feat: border 및 radius 테마 추가
    
    * chore: emotion/react, emotion/styled 패키지 추가

---
+++ src/constants/theme.ts
@@ -0,0 +1,36 @@
+export const theme = {
+  color: {
+    grayScaleAlt: 'var(--grayscale-alt)',
+    grayScaleWhite: 'var(--grayscale-white)',
+    grayScale50: 'var(--grayscale-50)',
+    grayScale100: 'var(--grayscale-100)',
+    grayScale200: 'var(--grayscale-200)',
+    grayScale300: 'var(--grayscale-300)',
+    grayScale400: 'var(--grayscale-400)',
+    grayScale500: 'var(--grayscale-500)',
+    grayScaleBlack: 'var(--grayscale-black)',
+    blue100: 'var(--blue-100)',
+    blue500: 'var(--blue-500)',
+  },
+  fontSize: {
+    small: 'var(--font-size-small)',
+    regular: 'var(--font-size-regular)',
+    medium: 'var(--font-size-medium)',
+    large: 'var(--font-size-large)',
+  },
+  fontWeight: {
+    medium: 'var(--font-weight-medium)',
+    bold: 'var(--font-weight-bold)',
+  },
+  border: {
+    default: 'var(--border-default)',
+    bold: 'var(--border-bold)',
+  },
+  radius: {
+    default: 'var(--radius-default)',
+    circle: 'var(--radius-circle)',
+  },
+  shadow: {
+    drop: 'var(--drop-shadow)',
+  },
+} as const;
